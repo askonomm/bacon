d54ea22e58c8812da65abf9ed01b00a14eb3084e
Asko Nõmm <asko@bien.ee>
2021-11-16 01:37:57 -0300

    bump

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import { marky } from "https://deno.land/x/marky@v1.1.4/mod.ts";
+import { marky } from "https://deno.land/x/marky@v1.1.5/mod.ts";
 import Handlebars from "https://unpkg.com/handlebars-esm";
 import * as std from "https://deno.land/std@0.114.0/fs/mod.ts";
 import * as testing from "https://deno.land/std@0.114.0/testing/asserts.ts";
