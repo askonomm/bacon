ad9dbea90b1e1e8d9ae6908a5470e16805f5b273
Asko Nõmm <asko@bien.ee>
2021-11-18 14:55:15 -0300

    bump

--- helpers.ts
+++ helpers.ts
@@ -14,9 +14,26 @@ function formatDate(input: string, data: any): string {
   return dtf.format(date);
 }
 
+// deno-lint-ignore no-explicit-any
+function date(format: string, data: any): string {
+  if (!format) {
+    return "{invalid_date_format}";
+  }
+
+  const date = new Date();
+  const locale = data.hash.locale ?? "en-US";
+  const opts = format ? JSON.parse(format) : undefined;
+  const dtf = Intl.DateTimeFormat(locale, opts);
+
+  return dtf.format(date);
+}
+
 const helpers: TemplateHelper[] = [{
   name: "format_date",
   fn: formatDate,
+}, {
+  name: "date",
+  fn: date
 }];
 
 export default helpers;
